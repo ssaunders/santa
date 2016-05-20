f445088bf283ba8aad55434abe7778f693e37088
ssaunders <stersaun@gmail.com>
2016-05-19 21:48:28 -0600

    Update santa.js
    adding inputs

--- santa.js
+++ santa.js
@@ -1,27 +1,29 @@
-function start(directions, location) {    
-  var houseCount = 1; //for spot he's at
-  var houseMap = {};
-  var hash;
-  for (var iter = 0; iter < directions.length; ++iter) {
-    switch(dir) {
-      case '>':
-        location.x++;
-        break;
-      case '<':
-        location.x--;
-        break;
-      case 'v':
-        location.y--;
-        break; 
-      case '^':
-        location.y++;
-        break;
-    }
-    hash = location.y+','+location.x;
-    if(!houseMap[hash]) {
-      houseMap[hash]=true;    
-      houseCount++;
-    }
+'use strict';
+var fs = require('fs');
+var directions = fs.readFileSync('./input.txt');
+var houseCount = 1; //for spot he's at
+var houseMap = {};
+var location = {x:0,y:0}
+var hash;
+for (var iter = 0; iter < directions.length; ++iter) {
+  switch(dir) {
+    case '>':
+      location.x++;
+      break;
+    case '<':
+      location.x--;
+      break;
+    case 'v':
+      location.y--;
+      break; 
+    case '^':
+      location.y++;
+      break;
   }
-  return houseCount;
+  hash = location.y+','+location.x;
+  if(!houseMap[hash]) {
+    houseMap[hash]=true;    
+    houseCount++;
+  }
 }
+return houseCount;
